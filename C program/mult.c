b8ad930a94987c39799223206544cc4bc4c50c98
Pranalinandankar <pranalinandankar188@gmail.com>
2022-03-31 15:17:06 +0530

    All Program

---
+++ C program/mult.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+ 	int num1,num2,res;
+ 	printf("Enter any two number:");
+ 	scanf ("%d %d", &num1,&num2);
+ 	res=num1*num2;
+ 	printf("mult is=%d",res);
+ }
---
+++ C program/sub.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int num1,num2,res;
+	printf("Enter any two number;");
+	scanf("%d %d", &num1,&num2);
+	res= num1-num2;
+	printf("sub is=%d",res);
+	
+}
