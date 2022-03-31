5920fe6aa95fc621df24b2340ede69bb687ab556
Pranalinandankar <pranalinandankar188@gmail.com>
2022-03-31 20:01:03 +0530

    Parallelogram

---
+++ C program/parallelogram.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+ 	int parall,b,h;
+ 	printf("Enter b:");
+ 	scanf("%d",&b);
+ 	printf("Enter h:");
+ 	scanf("%d",&h);
+ 	parall=b*h;
+	 printf("parall is=%d",parall);
+	
+	  }
