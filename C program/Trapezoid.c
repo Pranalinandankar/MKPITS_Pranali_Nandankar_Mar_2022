e1a1c4121090f9afa89b12fe637cafd2d6ec8b96
Pranalinandankar <pranalinandankar188@gmail.com>
2022-04-01 13:20:23 +0530

    Trapezoid

---
+++ C program/Trapezoid.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int T,a,b,h;
+	printf("Enter a:");
+	scanf("%d",&a);
+	printf("Enter b:");
+	scanf("%d",&b);
+	printf("Enter h:");
+	scanf("%d",&h);
+	T=(a+b)*h/2;
+	printf("Trapezoid is=%d",T);
+	}
