4d78229f37bfbf3dac39b3678cdaf656f2786f15
Pranalinandankar <pranalinandankar188@gmail.com>
2022-04-01 12:38:20 +0530

    Rectangle

---
+++ C program/Rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int r,w,l;
+	printf("Enter w:");
+	scanf("%d",&w);
+    printf("Enter l:");
+	scanf("%d",&l);
+	r=w*l;
+	printf("rectangle is=%d",r);
+	}
