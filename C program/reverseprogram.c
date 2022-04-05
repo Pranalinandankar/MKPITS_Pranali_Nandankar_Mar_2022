d71e5ddd682eb9c85bedc6be6bdb909ca9974924
Pranalinandankar <pranalinandankar188@gmail.com>
2022-04-05 21:25:59 +0530

    Revrseprogram

---
+++ C program/reverseprogram.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	char char1='X';
+	char char2='M';
+	char char3='L';
+	printf("The revrse of %c%C%C is %c%c%c\n",
+	char1,char2,char3,
+	char3,char2,char1);
+	return(0);
+	
+}
