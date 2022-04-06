a49412b22a8ee78c87d24ee52e40e084b8a300a7
Pranalinandankar <pranalinandankar188@gmail.com>
2022-04-06 21:20:24 +0530

    sum of two integer

---
+++ C program/sum of two integer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,sum;
+	
+	printf("\ninput the 1st integer:");
+	scanf("%d",&a);
+	
+	printf("\ninput the 2nd integer:");
+	scanf("%d",&b);
+	
+	sum=a+b;
+	printf("sum of two integer=%d\n",sum);
+}
