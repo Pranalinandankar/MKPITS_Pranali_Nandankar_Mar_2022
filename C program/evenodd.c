db733471464ad85af3d09aa766f7619362db5d7e
Pranalinandankar <pranalinandankar188@gmail.com>
2022-04-07 15:27:50 +0530

    Evon Odd

---
+++ C program/evenodd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int num;
+	printf("Enter any number:");
+	scanf("%d",&num);
+	if(num%2==0)
+	{
+	
+	printf("%d is even",num);
+    }
+    else
+    {
+	printf("%d is odd",num);
+    }
+	
+	
+}
