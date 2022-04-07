920ab1741391fc13c7ef56e00c1d9346a5976335
Pranalinandankar <pranalinandankar188@gmail.com>
2022-04-07 15:24:55 +0530

    positive or negative

---
+++ C program/positiveornot.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num;
+	printf("Enter any number:");
+	scanf("%d",&num);
+	if(num>=0)
+	{
+		printf("Number is positive\n");
+		
+	}
+	else
+	{
+		printf("Number is negative\n");
+	}
+}
