c0b7772f71653942d1561b64d9764a87da124d28
Pranalinandankar <pranalinandankar188@gmail.com>
2022-04-08 13:57:30 +0530

    All even number

---
+++ C program/alleven.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+ 	int num,n;
+ 	printf("Enter Even number between to 50(inclusive):\n");
+ 	for(num=1; num<=50; num++)
+
+ 	if(num%2==0)
+ 	{
+ 		printf("%d",num);
+	 }
+
+ }
