81c5f481b40b37d90896ba6e5354b1ea193bc6fc
Pranalinandankar <pranalinandankar188@gmail.com>
2022-04-09 14:15:48 +0530

    Positive or negative

---
+++ C program/positive or negative.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int s,num,n1,n2,n3,n4,n5,sum;
+    printf("Enter n1:");
+ 	scanf("%d",&n1);
+ 	printf("Enter n2:");
+ 	scanf("%d",&n2);
+ 	printf("Enter n3:");
+ 	scanf("%d",&n3);
+ 	printf("Enter n4:");
+ 	scanf("%d",&n4);
+ 	printf("Enter n5:");
+ 	scanf("%d",&n5);
+ 	
+ 	if(n1<=0)
+ 	 {
+ 		printf("n1 is negative\n");
+	 }
+	 else
+	 {
+	 	printf("n1 is positive\n");
+	 } 
+	 
+	 if(n2<=0)
+ 	 {
+ 		printf("n2 is negative\n");
+	 }
+	 else 
+	 {
+	 	printf("n2 is positive\n");
+	 }
+	 
+	 if(n3<=0)
+ 	 {
+ 		printf("n3 is negative\n");
+	 }
+	 else
+	 {
+	 	printf("n3 is positive\n");
+	 } 
+	 
+	 if(n4<=0)
+ 	 {
+ 		printf("n4 is negative\n");
+	 }
+	 else
+	 {
+	 	printf("n4 is positive\n");
+	 } 
+	 
+	 if(n5<=0)
+ 	 {
+ 		printf("n5 is negative\n");
+	 }
+	 else
+	 {
+	 	printf("n5 is positive\n");
+	 }
+ 	
+}
