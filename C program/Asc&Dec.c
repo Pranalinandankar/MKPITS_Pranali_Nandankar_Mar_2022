d53c0a49573d5fe54b654c2afe11decde28d3a7c
Pranalinandankar <pranalinandankar188@gmail.com>
2022-04-11 20:15:24 +0530

    Ascending or Decending

---
+++ C program/Asc&Dec.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,  Asc,Dec;
+
+	printf("Enter two pairs value:\n");
+	scanf("%d%d",&a,&b);
+	
+	if (a!=b)
+	{
+		if(a<b)
+		{
+		
+		printf("Asc\n");
+	    }
+    else
+	{
+		printf("Dec\n");
+	}
+
+    }
+}
