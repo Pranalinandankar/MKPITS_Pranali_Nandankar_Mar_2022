ddb179911dfb89d7e44cd76e701e581954eb2774
Pranalinandankar <pranalinandankar188@gmail.com>
2022-04-11 19:40:30 +0530

    Armstrong value using while

---
+++ C program/arm while.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i=001;
+	int a,b,c,t,res;
+	
+	printf("All armstrong no. betn 1 to 1000:\n");
+	
+	while (i<=1000)
+	{
+		t=i;
+		
+		a=t%10;
+		t=t/10;
+		b=t%10;
+		t=t/10;
+		c=t%10;
+		
+		res=(a*a*a)+(b*b*b)+(c*c*c);
+		
+		if(i==res)
+		{
+			printf("%d\n",res);
+		}
+		i++;
+	}
+}
