49791fbab1cf852af8d6e96600887591618855d6
Pranalinandankar <pranalinandankar188@gmail.com>
2022-04-21 20:30:45 +0530

    ABCD program

---
+++ C program/ABCD pro.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	char r,c,n;
+	char A=65,D=68;
+	for(r=A;r<=D;r++)
+	{
+		for(c=A;c<=D;c++)
+		{
+			printf("%c",c);
+		}
+		printf("\n");
+	}
+}
