15a5ae614f6067930a863c2344ab9037e69b1638
Pranalinandankar <pranalinandankar188@gmail.com>
2022-05-01 16:35:52 +0530

    Type 2 count all Pos and neg program

---
+++ C program/Type211.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<conio.h>
+void count()
+{
+	int n;
+	printf("%d",n);
+}
+void fun()
+{
+	int n1,n2,n3,n4,n5,sum=0,nega=0;
+	printf("Enter n1:");
+	scanf("%d",&n1);
+	printf("Enter n2:");
+	scanf("%d",&n2);
+	printf("Enter n3:");
+	scanf("%d",&n3);
+	printf("Enter n4:");
+	scanf("%d",&n4);
+	printf("Enter n5:");
+	scanf("%d",&n5);
+	
+	if(n1>=0)
+	{
+		sum=sum+n1;
+		
+	}
+	else
+	{
+		nega=nega+n1;
+	}
+	if(n2>=0)
+	{
+		sum=sum+n2;
+	}
+	else
+	{
+		nega=nega+n2;
+	}
+	if(n3>=0)
+	{
+		sum=sum+n3;
+	}
+	else
+	{
+		nega=nega+n3;
+	}
+	if(n4>=0)
+	{
+		sum=sum+n4;
+	}
+	else
+	{
+		nega=nega+n4;
+	}
+	if(n5>=0)
+	{
+		sum=sum+n5;
+	}
+	else
+	{
+		nega=nega+n5;
+	}
+	
+     printf("count of all positive no. %d\n",sum);
+     printf("count of all negative no. %d\n",nega);
+
+}
+void main()
+{
+	fun();
+}
