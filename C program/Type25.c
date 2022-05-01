6ef9bcbd795bd5c6ad1c0e5c73aae6f6d1c02b32
Pranalinandankar <pranalinandankar188@gmail.com>
2022-05-01 15:48:21 +0530

    Type 2 # program

---
+++ C program/Type25.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+ void hash()
+ {
+ 	int n;
+ 	printf("%d",n);
+ }
+ void fun()
+ {
+ 	printf(" ########\n");
+	printf(" #      #\n");
+	printf(" #      #\n");
+	printf(" ########\n");
+	printf(" #\n");
+	printf(" #\n");
+	printf(" #\n");
+ }
+ void main()
+ {
+ 	fun();
+ }
