e2bdee9709d3dfd2aa60b4187761fd48548709cc
Pranalinandankar <pranalinandankar188@gmail.com>
2022-05-01 13:35:40 +0530

    Type 2 all even upto 50 program

---
+++ C program/Type 22.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void even(int n)
+{
+	int n1;
+	printf("%d",n1);
+}
+void fun()
+{
+	int num,n;
+ 	printf("Enter Even number between to 50(inclusive):\n");
+ 	for(num=1; num<=50; num++)
+
+ 	if(num%2==0)
+ 	{
+ 		printf("%d\n",num);
+	 }
+}
+void main()
+{
+	fun();
+}
