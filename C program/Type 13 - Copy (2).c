c94ff840b8d23fc010be7170db26a8098926be58
Pranalinandankar <pranalinandankar188@gmail.com>
2022-05-01 14:02:55 +0530

    Type 1 AB program

---
+++ C program/Type 13 - Copy (2).c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	ab();
+}
+
+void ab()
+{
+	char r,c;
+	char n;
+	printf("Enter the no of lines:");
+	scanf("%d",&n);
+	for(r=1;r<=n;r++)
+	{
+		for(c=1;c<=r;c++)
+	{
+		printf("%c",(c+64));
+	}
+	printf("\n");
+	}
+}
