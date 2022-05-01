8ebe7b3e8855f1c9837c28e8e26476df5a0fd094
Pranalinandankar <pranalinandankar188@gmail.com>
2022-05-01 16:49:08 +0530

    Type 1 Prime num program

---
+++ C program/Type113.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	fun();
+}
+void fun()
+{
+	int num,i,m=0;
+	printf("Enter any number:");
+	scanf("%d",&num);
+	i=1;
+	do
+	{
+		if(num%i==0)
+		{
+			m=m+1;
+		}
+		i++;
+	}	while(i<=num);
+	if(m==2)
+	{
+		printf("%d is prime number",num);
+	}
+	else
+	{
+		printf("%d is Not prime number",num);
+	}	
+}
