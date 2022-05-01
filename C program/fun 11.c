e41253ab1ccda9d967915f068ef82926e24ba758
Pranalinandankar <pranalinandankar188@gmail.com>
2022-05-01 12:41:24 +0530

    Tpye 1 fun of multiplication of 3 num

---
+++ C program/fun 11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ fun();
+}
+void fun()
+{
+	int a,b,c;
+	printf("Enter three number:\n");
+	scanf("%d%d%d",&a,&b,&c);
+	printf("multiplication=%d",a*b*c);
+}
