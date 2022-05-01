7b74005f4f94b50407dbcb35f37e93e5f4fb3c4c
Pranalinandankar <pranalinandankar188@gmail.com>
2022-05-01 16:21:00 +0530

    Type 2 DOB program

---
+++ C program/Type29.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void dob()
+{
+	int n;
+	printf("%d",n);
+}
+
+void fun()
+{
+	printf("Name:Pranali Nandankar\n");
+	printf("DOB:18 aug,2000\n");
+	printf("Mobile no.:9370191145");
+
+}
+void main()
+{
+	fun();
+}
