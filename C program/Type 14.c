cf29e8e55c1f6a528a6b4ea8023ebb4111b2f74d
Pranalinandankar <pranalinandankar188@gmail.com>
2022-05-01 14:09:20 +0530

    Type 1 armstrong program

---
+++ C program/Type 14.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	arm();
+}
+
+void arm()
+{
+		int i=001;
+	int a,b,c,t,res;
+	
+	printf("All armstrong no. betn 1 to 1000:\n");
+	
+	while (i<=1000)
+	{
+		t=i;
+		
+		a=t%10;
+		t=t/10;
+		b=t%10;
+		t=t/10;
+		c=t%10;
+		
+		res=(a*a*a)+(b*b*b)+(c*c*c);
+		
+		if(i==res)
+		{
+			printf("%d\n",res);
+		}
+		i++;
+	}
+}
