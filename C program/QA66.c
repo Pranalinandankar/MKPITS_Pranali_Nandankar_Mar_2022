c140ccad0a5d669c322bceb66b21b1325181a2c8
Pranalinandankar <pranalinandankar188@gmail.com>
2022-05-04 13:04:31 +0530

    A Q66

---
+++ C program/QA66.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+  char c;
+  printf(" [A-Z] \n");
+  for (c=65;c<=90;c++) 
+  {
+    printf(" %d ",c);
+  }
+  printf("\n \n[a-z] \n");
+  for (c=97;c<=122;c++) 
+  {
+    printf(" %d",c);
+  }
+
+
+}
+
