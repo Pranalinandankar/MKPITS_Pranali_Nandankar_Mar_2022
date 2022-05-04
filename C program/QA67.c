3a0a0d226957d1f8bf42685ca3d8470e128a2386
Pranalinandankar <pranalinandankar188@gmail.com>
2022-05-04 13:06:29 +0530

    A Q67

---
+++ C program/QA67.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("Enter 7 digit Number : ");
+    scanf("%d",&n);
+    printf( "\n output : " );
+
+    printf("%d  ",(n/1000000));
+    n = n-((n/1000000)*1000000); 
+	  
+    printf("%d  ",(n/100000));
+    n = n-((n/100000)*100000);
+        
+    printf("%d  ",(n/10000));
+    n= n -((n/10000)*10000);
+    
+    printf("%d  ",(n/1000));
+    n = n - ((n/1000)*1000);
+    
+    printf("%d  ",(n/100));
+    n = n - ((n/100)*100);
+    
+    printf("%d  ",(n/10));
+    n= n -((n/10)*10);  
+    
+    printf("%d\n",(n%10));
+    
+}
