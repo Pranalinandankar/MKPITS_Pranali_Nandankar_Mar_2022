763cac34280b4408c1c7e38e87e822a0c509b7d2
Pranalinandankar <pranalinandankar188@gmail.com>
2022-05-04 13:27:25 +0530

    A Q72

---
+++ C program/QA72.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n1,n2,n3,n4;
+	n1=-1+4*6;
+	n2=(35+5)%7;
+	n3=14+(-4*6)/11;
+	n4=2+15/6*1-7%2;
+	printf(" Result= %d\n Result= %d\n Result= %d\n Result= %d\n",n1,n2,n3,n4);
+}
+
