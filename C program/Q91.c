c294ec3a748e1618796bbc781abb27b141386c69
Pranalinandankar <pranalinandankar188@gmail.com>
2022-05-10 19:34:37 +0530

    Q91.range 10.20 return 30_A91

---
+++ C program/Q91.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,sum;
+	printf("Enter two integer a,b:\n");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	
+	if(sum>=10 && sum<=20 ? 30:sum)
+	{
+		printf("30");		
+	}
+}
