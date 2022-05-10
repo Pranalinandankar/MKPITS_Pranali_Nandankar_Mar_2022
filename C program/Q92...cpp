a2cf63b6664fc14ab4476cca29e55e5283012d01
Pranalinandankar <pranalinandankar188@gmail.com>
2022-05-10 20:18:19 +0530

    True if either 5_AQ92

---
+++ C program/Q92...cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(void)
+{
+  int a,b,sum,div,res;
+  printf("Enter integer a:");
+  scanf("%d",&a);
+  printf("Enter integer b:");
+  scanf("%d",&b);
+  
+  if(a==5 || b==5 || sum==5 || (div)==5)
+  {
+  	printf("True");
+   } 
+   else
+   {
+   	printf("False");
+   }
+    }
