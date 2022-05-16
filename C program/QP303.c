65360ea3ba29576faea88cc052b59a656220ad14
Pranalinandankar <101799671+Pranalinandankar@users.noreply.github.com>
2022-05-16 13:52:25 +0530

    Pattern 55555_AQ303.

---
+++ C program/QP303.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int r,c;
+	for(r=5;r>=1;r--)
+	{
+		for(c=1;c<=5;c++)
+		{
+			if(r+c>5)
+			{
+			printf("%d",r);	
+			}
+			else
+			{
+			printf(" ");	
+			}
+
+		}
+		printf("\n");	
+	}	
+}
